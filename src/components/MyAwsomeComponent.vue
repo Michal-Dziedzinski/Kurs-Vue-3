94bc430f9d212840325baa7e8fc2b67be2437901
Michal-Dziedzinski <michal.marcin.dziedzinski@gmail.com>
2020-03-24 18:23:16 +0100

    Code after 1 episode - functions

---
+++ src/components/MyAwsomeComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="sayHello">Say hello</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyAwsomeComponent",
+  setup() {
+    function sayHello() {
+      const welcomeText = "Siema, co tam?";
+      console.log(addExclamationMark(welcomeText));
+    }
+
+    function addExclamationMark(text) {
+      return `${text}!`;
+    }
+    return { sayHello };
+  }
+};
+</script>
