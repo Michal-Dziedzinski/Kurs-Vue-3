1728b59626b46794b30f5fda2cb63efd95b31b64
Michal-Dziedzinski <michal.marcin.dziedzinski@gmail.com>
2020-04-22 10:16:52 +0200

    Code after 3 episode - reactive

--- src/components/MyAwsomeComponent.vue
+++ src/components/MyAwsomeComponent.vue
@@ -1,25 +1,40 @@
 <template>
+  <!-- <div>
+    <h1>Price: {{state.price}}$</h1>
+    <button @click="makeOrder">Make next order</button>
+    <p>Quantity: {{state.quantity}}</p>
+    <p>Order total price: {{state.totalPrice}}</p>
+    <p>Tax: {{state.tax}}</p>
+  </div>-->
   <div>
-    <button @click="incrementPension">Increment my pension</button>
-    <p>Your pension will be: {{pension}}</p>
+    <h1>Price: {{price}}$</h1>
+    <button @click="makeOrder">Make next order</button>
+    <p>Quantity: {{quantity}}</p>
+    <p>Order total price: {{totalPrice}}</p>
+    <p>Tax: {{tax}}</p>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive, toRefs } from "vue";
 
 export default {
   name: "MyAwsomeComponent",
   setup() {
-    const pension = ref(100);
+    const state = reactive({
+      quantity: 0,
+      price: 100,
+      totalPrice: 0,
+      tax: 0
+    });
 
-    console.log(pension);
-
-    function incrementPension() {
-      pension.value++;
+    function makeOrder() {
+      state.quantity++;
+      state.totalPrice = state.quantity * state.price;
+      state.tax = state.totalPrice * 0.23;
     }
 
-    return { pension, incrementPension };
+    return { makeOrder, ...toRefs(state) };
   }
 };
 </script>
