d83c8dcc93654bff2884a39bc31dfe8a092c2e0a
Michal-Dziedzinski <michal.marcin.dziedzinski@gmail.com>
2020-04-26 16:40:03 +0200

    Code after 4 episode - computed

--- src/components/MyAwsomeComponent.vue
+++ src/components/MyAwsomeComponent.vue
@@ -1,11 +1,4 @@
 <template>
-  <!-- <div>
-    <h1>Price: {{state.price}}$</h1>
-    <button @click="makeOrder">Make next order</button>
-    <p>Quantity: {{state.quantity}}</p>
-    <p>Order total price: {{state.totalPrice}}</p>
-    <p>Tax: {{state.tax}}</p>
-  </div>-->
   <div>
     <h1>Price: {{price}}$</h1>
     <button @click="makeOrder">Make next order</button>
@@ -16,7 +9,7 @@
 </template>
 
 <script>
-import { reactive, toRefs } from "vue";
+import { computed, toRefs, reactive } from "vue";
 
 export default {
   name: "MyAwsomeComponent",
@@ -24,14 +17,12 @@ export default {
     const state = reactive({
       quantity: 0,
       price: 100,
-      totalPrice: 0,
-      tax: 0
+      totalPrice: computed(() => state.price * state.quantity),
+      tax: computed(() => state.totalPrice * 0.23)
     });
 
     function makeOrder() {
       state.quantity++;
-      state.totalPrice = state.quantity * state.price;
-      state.tax = state.totalPrice * 0.23;
     }
 
     return { makeOrder, ...toRefs(state) };
