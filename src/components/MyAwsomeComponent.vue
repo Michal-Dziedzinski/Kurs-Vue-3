bacb9ab02d73f3a8d78819b0be7c7c14e7b8b903
Michal-Dziedzinski <michal.marcin.dziedzinski@gmail.com>
2020-05-27 14:13:11 +0200

    Code after 8 episode - v-for

--- src/components/MyAwsomeComponent.vue
+++ src/components/MyAwsomeComponent.vue
@@ -1,104 +1,132 @@
 <template>
-  <div>
-    <div>
-      <p>Masks: {{ masks }}</p>
-      <p v-if="masks > 3">You can buy a mask!</p>
-      <p v-else-if="masks > 0 && masks <= 3">
-        You can buy a mask, but hurry up!
-      </p>
-      <p v-else>You can't buy a mask, it's out of stock!</p>
-      <button
-        class="btn"
-        @click="buyMask"
-        :disabled="!masks"
-        :class="{ 'btn--warning': masks > 0 && masks <= 3 }"
-      >
-        Buy a mask
-      </button>
-    </div>
-    <div>
-      <img
-        :src="images[currentImage]"
-        :alt="`Image ${currentImage + 1}`"
-        class="image"
-      />
-      <button
-        class="btn"
-        @click="changeImage"
-        :disabled="currentImage >= images.length - 1"
-      >
-        Change Image
-      </button>
+  <div class="candidates">
+    <div
+      v-for="(candidate, index) in candidatesWithParty"
+      :key="candidate.id"
+      class="candidates__card"
+    >
+      <p>Candidate {{ index + 1 }}</p>
+      <p>Name: {{ candidate.firstName }} {{ candidate.lastName }}</p>
+      <p>Age: {{ candidate.age }}</p>
+      <p v-if="candidate.party">Party: {{ candidate.party }}</p>
+      <div v-if="candidate.opinionPool">
+        Opinion pool:
+        <progress max="100" :value="candidate.opinionPool"></progress>
+        {{ candidate.opinionPool }}%
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive, toRefs } from 'vue';
+import { ref, computed } from 'vue';
 
 export default {
   name: 'MyAwsomeComponent',
   setup() {
-    const masks = ref(5);
-
-    const styles = reactive({
-      btn: {
-        backgroundColor: '#17a2b8',
-        color: '#fff',
+    const candidatesWithParty = computed(() =>
+      candidates.value.filter(({ party }) => party),
+    );
+    const candidates = ref([
+      {
+        id: 1,
+        firstName: 'Robert',
+        lastName: 'Biedroń',
+        age: 44,
+        party: 'Wiosna Roberta Biedronia',
+        opinionPool: 4,
       },
-    });
-
-    function buyMask() {
-      masks.value--;
-    }
-
-    const images = ref([
-      'https://dziedziuch.samurajprogramowania.pl/projects/1-slider/images/1.jpg',
-      'https://dziedziuch.samurajprogramowania.pl/projects/1-slider/images/2.jpg',
-      'https://dziedziuch.samurajprogramowania.pl/projects/1-slider/images/3.jpg',
-      'https://dziedziuch.samurajprogramowania.pl/projects/1-slider/images/4.jpg',
-      'https://dziedziuch.samurajprogramowania.pl/projects/1-slider/images/5.jpg',
+      {
+        id: 2,
+        firstName: 'Krzysztof',
+        lastName: 'Bosak',
+        age: 37,
+        party: 'Konfederacja Wolność i Niepodległość',
+        opinionPool: 7,
+      },
+      {
+        id: 3,
+        firstName: 'Andrzej',
+        lastName: 'Duda',
+        age: 47,
+        party: null,
+        opinionPool: 49,
+      },
+      {
+        id: 4,
+        firstName: 'Szymon',
+        lastName: 'Hołownia',
+        age: 43,
+        party: null,
+        opinionPool: 22,
+      },
+      {
+        id: 5,
+        firstName: 'Marek',
+        lastName: 'Jakubiak',
+        age: 61,
+        party: 'Federacja dla Rzeczypospolitej',
+        opinionPool: null,
+      },
+      {
+        id: 6,
+        firstName: 'Małgorzata',
+        lastName: 'Kidawa-Błońska',
+        age: 63,
+        party: 'Platforma Obywatelska',
+        opinionPool: 5,
+      },
+      {
+        id: 7,
+        firstName: 'Władysław',
+        lastName: 'Kosiniak-Kamysz',
+        age: 38,
+        party: 'Polskie Stronnictwo Ludowe',
+        opinionPool: 12,
+      },
+      {
+        id: 8,
+        firstName: 'Mirosław',
+        lastName: 'Piotrowski',
+        age: 64,
+        party: 'Ruch Prawdziwa Europa',
+        opinionPool: null,
+      },
+      {
+        id: 9,
+        firstName: 'Paweł',
+        lastName: 'Tanajno',
+        age: 44,
+        party: null,
+        opinionPool: 1,
+      },
+      {
+        id: 10,
+        firstName: 'Stanisław',
+        lastName: 'Żółtek',
+        age: 64,
+        party: 'Kongres Nowej Prawicy',
+        opinionPool: null,
+      },
     ]);
 
-    const currentImage = ref(0);
-
-    function changeImage() {
-      currentImage.value++;
-    }
-
-    return {
-      masks,
-      buyMask,
-      ...toRefs(styles),
-      images,
-      currentImage,
-      changeImage,
-    };
+    return { candidates, candidatesWithParty };
   },
 };
 </script>
 
 <style lang="scss">
-.btn {
-  color: #fff;
-  background-color: #369b6d;
-  border: none;
-  padding: 5px 10px;
-  font-size: 16px;
-  cursor: pointer;
-  transition: background-color 0.1 ease-in;
-  &--warning {
-    background-color: #ffc107;
-    color: #000;
+.candidates {
+  font-family: arial;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 25px;
+  max-width: 1200px;
+  margin: 0 auto;
+
+  &__card {
+    border: 1px solid black;
+    padding: 10px;
   }
-  &:disabled {
-    cursor: default;
-    background-color: #bd2130;
-  }
-}
-.image {
-  margin-top: 50px;
-  width: 300px;
-  display: block;
 }
 </style>
