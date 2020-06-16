bfc8f9bc65ab62d3b07dd37ce52ea239f76a9aa6
Michal-Dziedzinski <michal.marcin.dziedzinski@gmail.com>
2020-06-16 07:56:46 +0200

    Code after 10 episode - lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div id="app">
-    <siemaApp />
     <MyAwsomeComponent />
   </div>
 </template>
 
 <script>
 import MyAwsomeComponent from './components/MyAwsomeComponent.vue';
-import siemaApp from './components/siemaApp';
 
 export default {
   name: 'App',
   components: {
     MyAwsomeComponent,
-    siemaApp,
   },
 };
 </script>
--- src/components/MyAwsomeComponent.vue
+++ src/components/MyAwsomeComponent.vue
@@ -1,110 +1,71 @@
 <template>
-  <ItemsList :items="candidates" />
+  <div>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">increment</button>
+    <MyAwsomeComponentChild v-if="count < 3" />
+  </div>
 </template>
 
 <script>
-import { ref, computed } from 'vue';
-import ItemsList from './items/ItemsList';
+import {
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  ref,
+  onRenderTracked,
+  onRenderTriggered,
+} from 'vue';
+
+import MyAwsomeComponentChild from './MyAwsomeComponentChild';
 
 export default {
   name: 'MyAwsomeComponent',
   components: {
-    ItemsList,
+    MyAwsomeComponentChild,
   },
   setup() {
-    const candidatesWithParty = computed(() =>
-      candidates.value.filter(({ party }) => party),
-    );
-    const candidates = ref([
-      {
-        id: 1,
-        firstName: 'Robert',
-        lastName: 'Biedroń',
-        age: 44,
-        party: 'Wiosna Roberta Biedronia',
-        opinionPool: 4,
-      },
-      {
-        id: 2,
-        firstName: 'Krzysztof',
-        lastName: 'Bosak',
-        age: '37',
-        party: 'Konfederacja Wolność i Niepodległość',
-        opinionPool: 7,
-      },
-      {
-        id: 3,
-        firstName: 'Andrzej',
-        lastName: 'Duda',
-        age: 47,
-        party: null,
-        opinionPool: 49,
-      },
-      {
-        id: 4,
-        firstName: 'Szymon',
-        lastName: 'Hołownia',
-        age: 43,
-        party: null,
-        opinionPool: 22,
-      },
-      {
-        id: 5,
-        firstName: 'Marek',
-        lastName: 'Jakubiak',
-        age: 61,
-        party: 'Federacja dla Rzeczypospolitej',
-        opinionPool: null,
-      },
-      {
-        id: 6,
-        firstName: 'Małgorzata',
-        lastName: 'Kidawa-Błońska',
-        age: 63,
-        party: 'Platforma Obywatelska',
-        opinionPool: 5,
-      },
-      {
-        id: 7,
-        firstName: 'Władysław',
-        lastName: 'Kosiniak-Kamysz',
-        age: 38,
-        party: 'Polskie Stronnictwo Ludowe',
-        opinionPool: 12,
-      },
-      {
-        id: 8,
-        firstName: 'Mirosław',
-        lastName: 'Piotrowski',
-        age: 64,
-        party: 'Ruch Prawdziwa Europa',
-        opinionPool: null,
-      },
-      {
-        id: 9,
-        firstName: 'Paweł',
-        lastName: 'Tanajno',
-        age: 44,
-        party: null,
-        opinionPool: 1,
-      },
-      {
-        id: 10,
-        firstName: 'Stanisław',
-        lastName: 'Żółtek',
-        age: 64,
-        party: 'Kongres Nowej Prawicy',
-        opinionPool: null,
-      },
-    ]);
+    const count = ref(0);
+    function increment() {
+      count.value++;
+    }
+    console.log('setup');
 
-    return { candidates, candidatesWithParty };
+    onBeforeMount(() => {
+      console.log('onBeforeMount');
+    });
+    onMounted(() => {
+      console.log('onMounted');
+    });
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate');
+    });
+    onUpdated(() => {
+      console.log('onUpdated');
+    });
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount');
+    });
+    onUnmounted(() => {
+      console.log('onUnmounted');
+    });
+
+    // onActivated
+    // onDeactivated
+
+    // onErrorCaptured
+
+    onRenderTracked((e) => {
+      console.log('onRenderTracked', e);
+    });
+    onRenderTriggered((e) => {
+      console.log('onRenderTriggered', e);
+    });
+    return { count, increment };
   },
 };
 </script>
 
-<style lang="scss">
-.siema {
-  background-color: red;
-}
-</style>
+<style lang="scss"></style>
